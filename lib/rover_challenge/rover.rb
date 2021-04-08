0f4e1500b7a19a5d0142d47ef69cc609391d72ff
Derrick <barlow.dw@gmail.com>
2021-04-08 20:55:36 +0200

    Added move method to Rover.

--- lib/rover_challenge/rover.rb
+++ lib/rover_challenge/rover.rb
@@ -1,5 +1,14 @@
 module RoverChallenge
     class Rover
+        DIRECTIONS = ['N', 'E', 'S', 'W'].freeze
+        INSTRUCTIONS = ['M', 'L', 'R'].freeze
+        MOVEMENT_DELTA = {
+            'N' => {x: 0, y: 1},
+            'E' => {x: 1, y: 0},
+            'S' => {x: 0, y: -1},
+            'W' => {x: -1, y: 0}
+        }.freeze
+
         attr_reader :x, :y, :d
 
         def initialize(x, y, d)
@@ -7,7 +16,38 @@ module RoverChallenge
             @y = Integer(y)
             @d = String(d).upcase
 
-            raise ArgumentError.new('Invalid direction') unless ['N', 'E', 'S', 'W'].include? @d
+            raise ArgumentError.new('Invalid direction') unless DIRECTIONS.include? @d
+        end
+
+        def move(instruction)
+            instruction = String(instruction).upcase
+
+            raise ArgumentError.new('Invalid direction') unless INSTRUCTIONS.include? instruction
+
+            case instruction
+            when 'L'
+                rotate -1
+            when 'R'
+                rotate 1
+            when 'M'
+                move_forward
+            end
+        end
+
+        private
+
+        def rotate(direction)
+            i = DIRECTIONS.index(@d) + direction
+            i = 0 if i >= DIRECTIONS.length
+            i = DIRECTIONS.length - 1 if i < 0
+            @d = DIRECTIONS[i]
+        end
+
+        def move_forward
+            delta = MOVEMENT_DELTA[@d]
+
+            @x = @x + delta[:x]
+            @y = @y + delta[:y]
         end
     end
 end
--- spec/rover_challenge/rover_spec.rb
+++ spec/rover_challenge/rover_spec.rb
@@ -2,47 +2,150 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 module RoverChallenge
   describe Rover do
-    it 'should create Rover' do
-      rover = Rover.new(1, 2, 'N')
+    describe '#new' do
+      it 'should create Rover' do
+        rover = Rover.new(1, 2, 'N')
 
-      expect(rover.x).to eq 1
-      expect(rover.y).to eq 2
-      expect(rover.d).to eq 'N'
+        expect(rover.x).to eq 1
+        expect(rover.y).to eq 2
+        expect(rover.d).to eq 'N'
+      end
+
+      it 'should create Rover using string inputs only' do
+        rover = Rover.new('1', '2', 'N')
+
+        expect(rover.x).to eq 1
+        expect(rover.y).to eq 2
+        expect(rover.d).to eq 'N'
+      end
+
+      it 'should raise error for invalid coordinate types' do
+        expect { Rover.new('zero', '2', 'N') }.to raise_error(ArgumentError)
+        expect { Rover.new('1', 'zero', 'N') }.to raise_error(ArgumentError)
+        expect { Rover.new('', '2', 'N') }.to raise_error(ArgumentError)
+        expect { Rover.new('1', nil, 'N') }.to raise_error(TypeError)
+      end
+
+      it 'should truncate cordinates to integer values' do
+        rover = Rover.new(3.14, 2.71828, 'N')
+
+        expect(rover.x).to eq 3
+        expect(rover.y).to eq 2
+      end
+
+      it 'should raise error for invalid direction' do
+        expect { Rover.new('1', '2', 'Z') }.to raise_error(ArgumentError)
+        expect { Rover.new('1', '2', 0) }.to raise_error(ArgumentError)
+        expect { Rover.new('1', '2', nil) }.to raise_error(ArgumentError)
+      end
+
+      it 'should convert direction to upper case' do
+        expect(Rover.new(1, 2, 'n').d).to eq 'N'
+        expect(Rover.new(1, 2, 'e').d).to eq 'E'
+        expect(Rover.new(1, 2, 's').d).to eq 'S'
+        expect(Rover.new(1, 2, 'w').d).to eq 'W'
+      end
     end
 
-    it 'should create Rover using string inputs only' do
-      rover = Rover.new('1', '2', 'N')
+    describe '#move' do
+      it 'should raise error for invalid instruction' do
+        rover = Rover.new(1, 2, 'N')
 
-      expect(rover.x).to eq 1
-      expect(rover.y).to eq 2
-      expect(rover.d).to eq 'N'
-    end
+        expect { rover.move 'k' }.to raise_error(ArgumentError)
+        expect { rover.move 1 }.to raise_error(ArgumentError)
+      end
 
-    it 'should raise error for invalid coordinate types' do
-      expect { Rover.new('zero', '2', 'N') }.to raise_error(ArgumentError)
-      expect { Rover.new('1', 'zero', 'N') }.to raise_error(ArgumentError)
-      expect { Rover.new('', '2', 'N') }.to raise_error(ArgumentError)
-      expect { Rover.new('1', nil, 'N') }.to raise_error(TypeError)
-    end
+      it 'should rotate left' do
+        rover = Rover.new(1, 2, 'N')
+        rover.move 'L'
 
-    it 'should truncate cordinates to integer values' do
-      rover = Rover.new(3.14, 2.71828, 'N')
+        expect(rover.x).to eq 1
+        expect(rover.y).to eq 2
+        expect(rover.d).to eq 'W'
 
-      expect(rover.x).to eq 3
-      expect(rover.y).to eq 2
-    end
+        rover.move 'L'
 
-    it 'should raise error for invalid direction' do
-      expect { Rover.new('1', '2', 'Z') }.to raise_error(ArgumentError)
-      expect { Rover.new('1', '2', 0) }.to raise_error(ArgumentError)
-      expect { Rover.new('1', '2', nil) }.to raise_error(ArgumentError)
-    end
+        expect(rover.d).to eq 'S'
 
-    it 'should convert direction to upper case' do
-      expect(Rover.new(1, 2, 'n').d).to eq 'N'
-      expect(Rover.new(1, 2, 'e').d).to eq 'E'
-      expect(Rover.new(1, 2, 's').d).to eq 'S'
-      expect(Rover.new(1, 2, 'w').d).to eq 'W'
+        rover.move 'L'
+
+        expect(rover.d).to eq 'E'
+
+        rover.move 'L'
+
+        expect(rover.d).to eq 'N'
+      end
+
+      it 'should rotate left with lower case' do
+        rover = Rover.new(1, 2, 'N')
+        rover.move 'l'
+
+        expect(rover.d).to eq 'W'
+      end
+
+      it 'should rotate right' do
+        rover = Rover.new(1, 2, 'N')
+        rover.move 'R'
+
+        expect(rover.x).to eq 1
+        expect(rover.y).to eq 2
+        expect(rover.d).to eq 'E'
+
+        rover.move 'R'
+
+        expect(rover.d).to eq 'S'
+
+        rover.move 'R'
+
+        expect(rover.d).to eq 'W'
+
+        rover.move 'R'
+
+        expect(rover.d).to eq 'N'
+      end
+
+      it 'should rotate right with lower case' do
+        rover = Rover.new(1, 2, 'N')
+        rover.move 'r'
+
+        expect(rover.d).to eq 'E'
+      end
+
+      it 'should move North' do
+        rover = Rover.new(1, 2, 'N')
+        rover.move 'M'
+
+        expect(rover.x).to eq 1
+        expect(rover.y).to eq 3
+        expect(rover.d).to eq 'N'
+      end
+
+      it 'should move East' do
+        rover = Rover.new(1, 2, 'E')
+        rover.move 'M'
+
+        expect(rover.x).to eq 2
+        expect(rover.y).to eq 2
+        expect(rover.d).to eq 'E'
+      end
+
+      it 'should move South' do
+        rover = Rover.new(1, 2, 'S')
+        rover.move 'M'
+
+        expect(rover.x).to eq 1
+        expect(rover.y).to eq 1
+        expect(rover.d).to eq 'S'
+      end
+
+      it 'should move West' do
+        rover = Rover.new(1, 2, 'W')
+        rover.move 'M'
+
+        expect(rover.x).to eq 0
+        expect(rover.y).to eq 2
+        expect(rover.d).to eq 'W'
+      end
     end
   end
 end
